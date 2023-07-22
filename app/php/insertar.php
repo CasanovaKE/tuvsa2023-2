1db51e7c14115075451f1d5be8b3235b3bc9f435
pc universidad <20307028@utfv.edu.mx>
2023-07-21 18:24:32 -0600

    21072023

--- app/php/insertar.php
+++ app/php/insertar.php
@@ -12,12 +12,36 @@ $password = isset($_POST['password']) ? $_POST['password'] : '';
 $con = new SQLite3("../data/tuvsa.db") or die("problemas para conectar");
 
 // consulta a SQL
-$cs = $con -> query("INSERT INTO usuarios (nombre, correo, password) VALUES ('$nombre', '$correo', '$password')");
+$cs = $con -> query("INSERT INTO registro (nombre, correo, password) VALUES ('$nombre', '$correo', '$password')");
 
 $con -> close();
 
-echo '<script>alert("Datos Insertados")</script>';
-echo '<script>window.location="../../registro.html"</script>';
+//echo '<script>alert("Datos Insertados")</script>';
+//echo '<script>window.location="../../registro.html"</script>';
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/cdn.jsdelivr.net_npm_bootstrap@5.3.0_dist_css_bootstrap.min.css">
+    <title>Datos</title>
+</head>
+<body>
+    <script src="../../js/sweetalert2.js"></script>
+    <script>
+    Swal.fire({
+        title: 'Datos Guardados',
+        icon: 'success',
+        confirmButtonColor: '#3085d6',
+    }).then((result) => {
+            if (result.isConfirmed) {
+            window.location='../../registro.html'
+        }
+    })
+    </script>
+</body>
+</html>
